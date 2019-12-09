4481f6b6a0fa95bdab0ca1f116bca147101377fb
Maixent Chenebaux <max.chbx@gmail.com>
2019-12-09 10:07:11 +0100

    version 0.0.2 in CPP

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="linearity",
-    version="0.0.1",
+    version="0.0.2",
     author="Maixent Chenebaux",
     author_email="max.chbx@gmail.com",
     description="Linear algebra with minimal overhead",
